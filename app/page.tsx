3f26d3491406a8fada86693df50857b6952094b3
Murod Akhmedov <murod.london@gmail.com>
2024-12-02 09:29:37 +0000

    Fix layout of home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
 
-      <div className="max-w-3xl mx-auto my-14">
+      <div className="max-w-5xl mx-auto my-14">
         <MdView />
       </div>
     </>
